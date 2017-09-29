31fcb99dbd784e608a8734fdf596f5288e8fb143
DrWhue <erichuloveu@gmail.co>
2017-09-29 16:54:37 -0700

    My first push

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderAutonomous.java
@@ -9,7 +9,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 
-
+//Added a line of code
 @Autonomous(name="Autonomous with Encoder", group="Iterative Opmode")  // @Autonomous(...) is the other common choice
 
 public class EncoderAutonomous extends OpMode {
