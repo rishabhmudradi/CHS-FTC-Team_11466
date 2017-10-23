39ea037964d7693020900ccef3a7f1d2c24138ea
sreeharirammohan@gmail.com <wizmathhelp@gmail.com>
2017-10-23 15:55:03 -0700

    Sensor test code added

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SensorMRGyro_v1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SensorMRGyro_v1.java
@@ -52,7 +52,6 @@ import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
 */
 @TeleOp(name = "Sensor: MR Gyro v1", group = "Sensor")
-@Disabled
 public class SensorMRGyro_v1 extends LinearOpMode {
 
   /** In this sample, for illustration purposes we use two interfaces on the one gyro object.
@@ -76,7 +75,7 @@ public class SensorMRGyro_v1 extends LinearOpMode {
 
     // Get a reference to a Modern Robotics gyro object. We use several interfaces
     // on this object to illustrate which interfaces support which functionality.
-    modernRoboticsI2cGyro = hardwareMap.get(ModernRoboticsI2cGyro.class, "gyro");
+    modernRoboticsI2cGyro = hardwareMap.get(ModernRoboticsI2cGyro.class, "sensor_gyro");
     gyro = (IntegratingGyroscope)modernRoboticsI2cGyro;
     // If you're only interested int the IntegratingGyroscope interface, the following will suffice.
     // gyro = hardwareMap.get(IntegratingGyroscope.class, "gyro");
