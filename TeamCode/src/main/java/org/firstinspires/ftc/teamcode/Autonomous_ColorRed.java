15b6728af5f7123c75f62c750f32bb82b998550f
sreeharirammohan@gmail.com <wizmathhelp@gmail.com>
2017-10-30 16:24:34 -0700

    Added Red Autonomous Template

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous_ColorRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous_ColorRed.java
@@ -3,16 +3,20 @@ package org.firstinspires.ftc.teamcode;
 import android.graphics.Color;
 import android.view.View;
 
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cGyro;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.IntegratingGyroscope;
 import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
 import com.qualcomm.robotcore.hardware.NormalizedRGBA;
 import com.qualcomm.robotcore.hardware.SwitchableLight;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import static java.lang.Thread.sleep;
 
-@TeleOp(name = "Sensor: Color", group = "Sensor") // @Autonomous(...) is the other common choice
+
+@TeleOp(name = "Red", group = "Autonomous Version:") // @Autonomous(...) is the other common choice
 
 public class Autonomous_ColorRed extends OpMode{
     /* Declare OpMode members. */
@@ -23,6 +27,11 @@ public class Autonomous_ColorRed extends OpMode{
     View relativeLayout;
     private double start_time;
     private int TICKS_PER_REVOLUTION = 1120;
+    IntegratingGyroscope gyro;
+    ModernRoboticsI2cGyro modernRoboticsI2cGyro;
+
+    // A timer helps provide feedback while calibration is taking place
+    ElapsedTime timer = new ElapsedTime();
 
     /*
          * Code to run ONCE when the driver hits INIT
@@ -33,6 +42,9 @@ public class Autonomous_ColorRed extends OpMode{
 
         leftMotor = hardwareMap.dcMotor.get("left_drive"); //we would configure this in FTC Robot Controller app
         rightMotor = hardwareMap.dcMotor.get("right_drive");
+
+        modernRoboticsI2cGyro = hardwareMap.get(ModernRoboticsI2cGyro.class, "sensor_gyro");
+        gyro = (IntegratingGyroscope)modernRoboticsI2cGyro;
 
         leftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         rightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
@@ -48,6 +60,20 @@ public class Autonomous_ColorRed extends OpMode{
 
         //colorSensor = hardwareMap.colorSensor.get("name_of_color_sensor"); //we would configure the name of the color sensor later in the
         //ftc robot controller
+
+        while (modernRoboticsI2cGyro.isCalibrating())  {
+            telemetry.addData("calibrating", "%s", Math.round(timer.seconds())%2==0 ? "|.." : "..|");
+            telemetry.update();
+            try {
+                sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+
+
     }
     public void driveForward(double power, int distance){
         leftMotor.setMode(DcMotor.RunMode.RESET_ENCODERS);
@@ -120,10 +146,16 @@ public class Autonomous_ColorRed extends OpMode{
         leftMotor.setPower(power);
         rightMotor.setPower(-power);
     }
-    //Pass in true for right, false for right
-    public void hitBall(boolean direction){
-
+    //Pass in right for right, left for left
+    public void hitBall(String direction){
+        //move the servo the correct amount of degress.
     }
+
+    public void callibrate() {
+        //turn through an angle of 120 until we find the color
+    }
+
+
     @Override
     public void init_loop() {
 
@@ -133,27 +165,41 @@ public class Autonomous_ColorRed extends OpMode{
     public void start() {
         //this is a way to print to the screen of the iphone app, useful for debugging.
         start_time = System.currentTimeMillis();
-        telemetry.addData("Robot starting Will this work?", "");
+        telemetry.addData("Autonomous Color Red Starting", "");
+
+        //driveForward(to the ball);
+
+        //
+
         TurnRightDistance(0.25, convert_to_REV_distance(0, 1));
         while(true) {
             try {
-                switch (getColor()) {
-                    case "Red":
-                        hitBall(true);
-                        break;
-                    case "Blue":
-                        hitBall(true);
-                        break;
-                    case "Neither":
-                        hitBall(true);
-                        break;
+                String color = getColor();
+                if(color.equals("Red")) {
+                    //red is on the right
+                    hitBall("Right");
+                    break;
+                } else if(color.equals("Blue")) {
+                    //blue is on the left
+                    hitBall("Left");
+                    break;
+                } else {
+
+                    //recalibrate
+                    callibrate();
+
                 }
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+
         }
 
     }
+
+
+
+
     @Override
     public void loop() {
         telemetry.addData("Robot starting Will this work?", "");
