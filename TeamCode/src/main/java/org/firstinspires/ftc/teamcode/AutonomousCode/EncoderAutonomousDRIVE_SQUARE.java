30fc6622658448218c2dab3a740e2dd6c740e515
sreeharirammohan@gmail.com <wizmathhelp@gmail.com>
2017-11-07 23:55:19 -0800

    Organization

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousCode/EncoderAutonomousDRIVE_SQUARE.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousCode/EncoderAutonomousDRIVE_SQUARE.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.Sensor;
+package org.firstinspires.ftc.teamcode.AutonomousCode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
