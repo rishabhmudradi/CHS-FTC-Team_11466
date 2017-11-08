f9eeace93362abcd2a134c75e347ad20b6f3279e
sreeharirammohan@gmail.com <wizmathhelp@gmail.com>
2017-11-07 23:59:47 -0800

    remove disabled

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sensor/ConceptScanServo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sensor/ConceptScanServo.java
@@ -49,7 +49,6 @@ import com.qualcomm.robotcore.hardware.Servo;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 @Autonomous(name = "Concept: Scan Servo", group = "Concept")
-@Disabled
 public class ConceptScanServo extends LinearOpMode {
 
     static final double INCREMENT   = 0.01;     // amount to slew servo each CYCLE_MS cycle
