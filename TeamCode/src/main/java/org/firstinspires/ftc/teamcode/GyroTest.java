4da526b3e78d46eefe53d47ba19b5e78d3ed35dc
sreeharirammohan@gmail.com <wizmathhelp@gmail.com>
2017-11-07 23:42:32 -0800

    Pushing Gyro Test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GyroTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GyroTest.java
@@ -15,87 +15,59 @@ import com.qualcomm.robotcore.util.Range;
 @Autonomous(name= "Gyro Test", group = "Autonomous")
 public class GyroTest extends LinearOpMode {
 
-    static final double     HEADING_THRESHOLD       = 1 ;      // As tight as we can make it with an integer gyro
-    static final double     P_TURN_COEFF            = 0.1;     // Larger is more responsive, but also less stable
-    static final double     P_DRIVE_COEFF           = 0.15;     // Larger is more responsive, but also less stable
-
-    static final double     COUNTS_PER_MOTOR_REV    = 1120 ;    // eg: TETRIX Motor Encoder
-    static final double     DRIVE_GEAR_REDUCTION    = 0.75;     // This is < 1.0 if geared UP
-    static final double     WHEEL_DIAMETER_INCHES   = 3.5 ;     // For figuring circumference
-    static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
-            (WHEEL_DIAMETER_INCHES * 3.1415);
-
-    static final double     DRIVE_SPEED             = 0.7;     // Nominal speed for better accuracy.
-    static final double     TURN_SPEED              = 0.3;     // Nominal half speed for better accuracy.
-
-    ModernRoboticsI2cGyro gyro        = null;                    // Additional Gyro device
+    ModernRoboticsI2cGyro gyro        = null;
     DcMotor leftMotor   = null;
     DcMotor rightMotor  = null;
 
+    private ElapsedTime runtime = new ElapsedTime();
+
+    double degrees = 90;
+
     @Override
     public void runOpMode() throws InterruptedException {
-        /*
-         * Initialize the standard drive system variables.
-         * The init() method of the hardware class does most of the work here
-         */
         //robot.init(hardwareMap);
         gyro = (ModernRoboticsI2cGyro)hardwareMap.gyroSensor.get("sensor_gyro");
         leftMotor = (DcMotor) hardwareMap.dcMotor.get("left_drive");
         rightMotor = (DcMotor) hardwareMap.dcMotor.get("right_drive");
 
-        // Ensure the robot it stationary, then reset the encoders and calibrate the gyro.
         leftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         rightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-        // Send telemetry message to alert driver that we are calibrating;
-        telemetry.addData(">", "Calibrating Gyro");    //
+        leftMotor.setDirection(DcMotor.Direction.FORWARD);
+        rightMotor.setDirection(DcMotor.Direction.REVERSE);
+
+        telemetry.addData(">", "Calibrating Gyro");
         telemetry.update();
 
         gyro.calibrate();
 
-        // make sure the gyro is calibrated before continuing
-        while (gyro.isCalibrating())  {
-            Thread.sleep(50);
-            idle();
-        }
-
-        telemetry.addData(">", "Robot Ready.");    //
-        telemetry.update();
-
-        leftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        rightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-        // Wait for the game to start (Display Gyro value), and reset gyro before we move..
-        while (!isStarted()) {
-            telemetry.addData(">", "Robot Heading = %d", gyro.getIntegratedZValue());
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.addData("Gyro Direction", "Gyro angle: " + gyro.getHeading());
             telemetry.update();
-            idle();
+            turnTo(degrees);
+
+            if(degrees > 360){
+                degrees = 15;
+            }
+
+            degrees += 15;
         }
-        gyro.resetZAxisIntegrator();
 
-        // Step through each leg of the path,
-        // Note: Reverse movement is obtained by setting a negative distance (not speed)
-        // Put a hold after each turn
+    }
 
-        rightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+    public void turnTo(double degrees){
+        int turnBy = -1;                 //turns clockwise
 
-        //gyroDrive(DRIVE_SPEED, 12.0, 90.0);    // Drive FWD 48 inches
-        gyroTurn(TURN_SPEED, 65);         // Turn  CCW to -45 Degrees
-        //gyroDrive(DRIVE_SPEED, 12, 0);
+        if(degrees < gyro.getHeading()){
+            turnBy *= -1;
+        }
 
-
-
-
-//        gyroHold( TURN_SPEED, -45.0, 0.5);    // Hold -45 Deg heading for a 1/2 second
-//        gyroTurn( TURN_SPEED,  45.0);         // Turn  CW  to  45 Degrees
-//        gyroHold( TURN_SPEED,  45.0, 0.5);    // Hold  45 Deg heading for a 1/2 second
-//        gyroTurn( TURN_SPEED,   0.0);         // Turn  CW  to   0 Degrees
-//        gyroHold( TURN_SPEED,   0.0, 1.0);    // Hold  0 Deg heading for a 1 second
-//        gyroDrive(DRIVE_SPEED,-48.0, 0.0);    // Drive REV 48 inches
-//        gyroHold( TURN_SPEED,   0.0, 0.5);    // Hold  0 Deg heading for a 1/2 second
-
-        telemetry.addData("Path", "Complete");
-        telemetry.update();
+        while(degrees != gyro.getHeading()){
+            leftMotor.setPower(-turnBy);
+            rightMotor.setPower(turnBy);
+        }
     }
 
 }
